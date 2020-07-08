00897c0f8d048343d4f6d16e535309212b4ecf2c
Lucas Klein <59050136+lectair@users.noreply.github.com>
2020-07-08 23:25:17 +0200

    Create bruteviewer.py

---
+++ bruteviewer.py
@@ -0,0 +1,52 @@
+import requests
+import sys
+import time
+
+try:
+    media_code = sys.argv[1].split('/p/')[1].split('/')[0]
+    delay = int(sys.argv[2])+0
+    time.sleep(int(delay))
+    print(f'MEDIA CODE: {media_code}\nDELAY: {delay}\n\nProcess:')
+except IndexError:
+    print('Usage:    python bruteviewer.py video_link delay_in_seconds\nExample:  python bruteviewer.py https://www.instagram.com/p/CAbMALKg0VM/ 3\n')
+    sys.exit()
+except ValueError:
+    print('Please set a valid delay\nExample: python bruteviewer.py https://www.instagram.com/p/CAbMALKg0VM/ 3\n')
+    sys.exit()
+except NameError:
+    print('Usage:    python bruteviewer.py video_link delay_in_seconds\nExample:  python bruteviewer.py https://www.instagram.com/p/CAbMALKg0VM/ 3\n')
+    sys.exit()
+except:
+    print('Unknown exception ocurred.')
+
+headers = {
+    "Host":"sifresiz.instahile.co",
+    "Accept":"application/json, text/javascript, /; q=0.01",
+    "X-Requested-With":"XMLHttpRequest",
+    "Accept-Language":"es-es",
+    "Accept-Encoding":"gzip, deflate, br",
+    "Content-Type":"application/x-www-form-urlencoded; charset=UTF-8",
+    "Origin":"https://sifresiz.instahile.co",
+    "User-Agent":"LMAOOO",
+    "Connection":"keep-alive",
+    "Referer":"https://sifresiz.instahile.co/views",
+    "Content-Length":"34",
+    "Cookie":"__cfduid=da3b912f0adc7e45eebba4568f47299991594233976; _ga=GA1.2.8790189.1594221068; _gat_gtag_UA_129054916_2=1; _gid=GA1.2.1485195277.1594221068"
+}
+
+params = {
+    "media_code": media_code,
+    "quantity": 30
+}
+
+while True:
+    request = requests.post("https://sifresiz.instahile.co/c/views.php", data=params, headers=headers)
+    response = request.text
+    if "�" in response:
+        print('Captcha!')
+    elif "503 Service" in response:
+        print('Error 503. Wait a minute while we reload...')
+        time.sleep(15)
+    else:
+        print(response)
+        time.sleep(int(delay))
